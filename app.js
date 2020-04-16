306e5cbe4493ece4c6315da552a8a63b5116ecb8
Walter Celiz <walterceliz93@gmail.com>
2020-04-15 22:10:09 -0300

    implement routing for contact and about pages

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ var http = require('http');
 //creando server minimo
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1> Hello World con HTML </h1>");
-    res.end();
+
+    var url = req.url;
+
+    if(url === '/contact') {
+        res.write("<h1>Contacto</h1>");
+        res.end();
+    }else if(url === '/about') {
+        res.write("<h1>Acerca de</h1>");
+        res.end();
+    }else {
+        res.write("<h1>Bienvenido</h1>");
+        res.end();
+    }        
+    
 }).listen(3000, function() {
     console.log("servidor escuchando puerto 3000")
 });
